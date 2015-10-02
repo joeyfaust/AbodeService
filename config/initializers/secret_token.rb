16c1510df81d03d80ba1e4ccb2f2faeaf1e2ccfb
joeyfaust <onetwosc@box534.bluehost.com>
2015-10-02 12:22:42 -0600

    Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbodeService::Application.config.secret_token = '2aa360d964706895a542bd4cfbfee0e8715da6cc7c3007405a11d7b78f8c8328e35844132237018adcc373f07850e84201a21d1316d04648aa4fa1af067f7240'
